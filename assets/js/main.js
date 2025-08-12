341ada6f4e893b0be6282329d300ffb8380d57e1
puyokura <puyokura@users.noreply.github.com>
2025-08-12 12:48:24 +0000

    deploy: 87aecf46a6f058d83a6b26e6e46b5510d2aea486

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,28 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  // すべてのハイライトされたコードブロックを取得
-  const highlights = document.querySelectorAll('div.highlight');
+// Main JavaScript file for custom interactions
 
-  highlights.forEach((div) => {
-    // コードブロックをラップするコンテナを作成
+document.addEventListener('DOMContentLoaded', function () {
+  // Add copy button to code blocks
+  const highlights = document.querySelectorAll('.highlight');
+  highlights.forEach(function (highlight) {
     const container = document.createElement('div');
-    container.style.position = 'relative';
-    div.parentNode.insertBefore(container, div);
-    container.appendChild(div);
+    container.classList.add('position-relative');
+    highlight.parentNode.insertBefore(container, highlight);
+    container.appendChild(highlight);
 
-    // コピーボタンを作成
-    const copyButton = document.createElement('button');
-    copyButton.className = 'copy-btn';
-    copyButton.textContent = 'Copy'; // アイコンの代わりにテキストを使用（後でCSSでアイコンに置き換えも可能）
-    container.appendChild(copyButton);
+    const button = document.createElement('button');
+    button.classList.add('btn-copy', 'btn', 'btn-sm', 'position-absolute', 'top-2', 'right-2');
+    button.setAttribute('aria-label', 'Copy to clipboard');
+    button.innerHTML = `<svg class="octicon octicon-copy" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>`;
 
-    copyButton.addEventListener('click', () => {
-      const code = div.querySelector('pre code').innerText;
-      navigator.clipboard.writeText(code).then(() => {
-        copyButton.textContent = 'Copied!';
-        setTimeout(() => {
-          copyButton.textContent = 'Copy';
+    container.appendChild(button);
+
+    button.addEventListener('click', function () {
+      const code = highlight.querySelector('pre code').innerText;
+      navigator.clipboard.writeText(code).then(function () {
+        button.innerHTML = `<svg class="octicon octicon-check color-fg-success" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>`;
+        setTimeout(function () {
+          button.innerHTML = `<svg class="octicon octicon-copy" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill-rule="evenodd" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path></svg>`;
         }, 2000);
-      }, (err) => {
-        copyButton.textContent = 'Failed';
-        console.error('Could not copy text: ', err);
       });
     });
   });
